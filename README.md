1a1fc92edebc9f6beec86e8491e07479733c8ce3
artuste <artuste@gmail.com>
2015-06-30 11:51:20 +0200

    Init.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# angular2App
+# AngularJS 2 App
+
+<pre>$ npm install -g tsd@^0.6.0</pre>
+<pre>$ tsd install angular2 es6-promise rx rx-lite</pre>
+
+<h2>Terminal 1</h2>
+<pre>$ tsc --watch -m commonjs -t es5 --emitDecoratorMetadata app.ts</pre>
+
+<h2>Terminal 2</h2>
+<pre>$ http-server</pre>
---
+++ app.ts
@@ -0,0 +1,28 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+
+/*
+ If IDE compiler doesn't work
+
+ terminal:
+ tsc --watch -m commonjs -t es5 --emitDecoratorMetadata app.ts
+ */
+
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: '<h1>Hello {{ name }}</h1>'
+})
+
+// Component controller
+class MyAppComponent {
+    name: string;
+
+    constructor() {
+        this.name = 'Alice23a';
+    }
+}
+
+bootstrap(MyAppComponent);
